3569e8d3cb3f726c8024a8bab5bf73b36d8fb0cb
rogiersbart <brogiers@sckcen.be>
2015-05-18 17:22:58 +0200

    Modification of run_mt3dms input parameters

--- R/run_mt3dms.R
+++ R/run_mt3dms.R
@@ -5,7 +5,7 @@
 #' @param file Filename; typically *.btn
 #' @return Object of class btn
 #' @export
-run_mt3dms <- function(nam,dir=getwd(),mt3dmsVersion='mt3dms5b')
+run_mt3dms <- function(nam,dir=getwd(),mt3dms_executable='mt3dms5b')
 {
-  shell(paste('cd',dir,'&',mt3dmsVersion,nam)) 
+  shell(paste('cd',dir,'&',mt3dms_executable,nam)) 
 }
