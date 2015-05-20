0eddf38ebbb80ee0abffa274a76002b9a8f67053
rogiersbart <brogiers@sckcen.be>
2015-05-20 15:34:25 +0200

    Adjusted run_mt3dms input parameters, for reenabling path autocompletion

--- R/run_mt3dms.R
+++ R/run_mt3dms.R
@@ -1,11 +1,12 @@
 #' Run an MT3DMS model
 #' 
-#' \code{read.btn} reads in an MT3DMS basic transport package file and returns it as an \code{\link{RMT3DMS}} btn object.
+#' \code{run_mt3dms} runs an MT3DMS model.
 #' 
-#' @param file Filename; typically *.btn
-#' @return Object of class btn
+#' @param file Path to name file; typically "*.nam"
 #' @export
-run_mt3dms <- function(nam,dir=getwd(),mt3dms_executable='mt3dms5b')
+run_mt3dms <- function(file,mt3dms_executable='mt3dms5b')
 {
-  shell(paste('cd',dir,'&',mt3dms_executable,nam)) 
+  dir <- dirname(file)
+  file <- basename(file)
+  shell(paste('cd',dir,'&',mt3dms_executable,file),mustWork=TRUE) 
 }
